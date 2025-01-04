4513d5aece2886c5bb5c5512d20ef04c745b58f6
Ali Berkay Celik <aliberkaycelik@hotmail.com>
2025-01-04 23:46:18 +0300

    Add project files.

---
+++ TravelTripProje/Controllers/ContactController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProje.Models.Siniflar;
+
+namespace TravelTripProje.Controllers
+{
+    public class ContactController : Controller
+    {
+		// GET: Contact
+		Context c = new Context();
+		[HttpGet]
+        public ActionResult Index()
+        {
+			
+			return View();
+        }
+        [HttpPost]
+        public ActionResult Index(iletisim y)
+        {
+            c.iletisims.Add(y);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
